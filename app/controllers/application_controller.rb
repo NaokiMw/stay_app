168c2d8e53d6ef990375fa9a18a3083f1540a4e0
maekawanaoki <personalpeach10@icloud.com>
2022-03-01 18:37:43 +0900

    ユーザーのプロフィール編集の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
     #deviseのストロングパラメータをnameに変更した為、Emailをストロングパラメータに追加
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+      # アカウント編集の時にnameとprofileのストロングパラメータを追加
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
     end
 
 end
